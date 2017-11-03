643ac5ef2f4b3d2ab18b71c6b69b60319e93d6c2
adminarun <arun14uit007@gmail.com>
2017-11-03 23:54:24 +0530

    Main File Has Been Added !

---
+++ README.md
@@ -0,0 +1,3 @@
+# This is a shell Creator 
+
+## Developer Err0R - dEfc6 @Team$Hck4$ 
---
+++ main.py
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# --- imports ------
+import os
+#-------------------
+#------variables --------
+
+#-------------------
+
+def main():
+    print 'Help'
+   
+
+if __name__ =='__main__':
+    main()
